92c6ecd324db7530b75196644ed4f2405089afdc
Samuel <samuelljason@gmail.com>
2023-11-01 17:10:05 -0300

    Estudo do red out e in

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -4,9 +4,35 @@
     {
         static void Main(string[] args)
         {
-            
-           
+            int valor;
+            DoSomething(out valor);
 
+            Console.WriteLine(valor);
+            
+            void DoSomething(out int number) //daqui o valor sobe para variavel valor
+            {
+                number = 123;
+            }
+
+
+            void Increment(ref int number)
+            {
+                number++;
+            }
+
+            int number = 1;
+            Increment(ref number);
+            Console.WriteLine(number);
+
+
+
+            static void UseParams(params int[] items)
+            {
+                foreach (var item in items)
+                {
+                    Console.WriteLine(item);
+                }
+            }
         }
     }
 }
